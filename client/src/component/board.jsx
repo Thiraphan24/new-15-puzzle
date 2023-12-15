f86d856f4bf03c98ada8ecc852ecaa0d593d76cd
Thiraphan24 <thiraphan.codercamp@gmail.com>
2023-12-15 22:18:51 +0700

    style timer

--- client/src/component/board.jsx
+++ client/src/component/board.jsx
@@ -3,26 +3,27 @@ import "./board.css";
 
 const Board = () => {
   const [puzzle, setPuzzle] = useState([
-    <img src="1.jpg" />,
-    <img src="2.jpg" />,
-    <img src="3.jpg" />,
-    <img src="4.jpg" />,
-    <img src="5.jpg" />,
-    <img src="6.jpg" />,
-    <img src="7.jpg" />,
-    <img src="8.jpg" />,
-    <img src="9.jpg" />,
-    <img src="10.jpg" />,
-    <img src="11.jpg" />,
-    <img src="12.jpg" />,
-    <img src="13.jpg" />,
-    <img src="14.jpg" />,
-    <img src="15.jpg" />,
+    <img src="1.jpg" alt="1" />,
+    <img src="2.jpg" alt="2" />,
+    <img src="3.jpg" alt="3" />,
+    <img src="4.jpg" alt="4" />,
+    <img src="5.jpg" alt="5" />,
+    <img src="6.jpg" alt="6" />,
+    <img src="7.jpg" alt="7" />,
+    <img src="8.jpg" alt="8" />,
+    <img src="9.jpg" alt="9" />,
+    <img src="10.jpg" alt="10" />,
+    <img src="11.jpg" alt="11" />,
+    <img src="12.jpg" alt="12" />,
+    <img src="13.jpg" alt="13" />,
+    <img src="14.jpg" alt="14" />,
+    <img src="15.jpg" alt="15" />,
     "Empty",
   ]);
   const [time, setTime] = useState(0);
   const [score, setScore] = useState(0);
   const [rank, setRank] = useState([]);
+  const [paused, setPaused] = useState(true);
 
   // สุ่มจัดเรียงรูป
   useEffect(() => {
@@ -31,13 +32,92 @@ const Board = () => {
 
   // จับเวลา
   useEffect(() => {
-    const interval = setInterval(() => {
-      setTime((prevTime) => prevTime + 1);
-    }, 1000);
+    let interval;
+    if (!paused) {
+      interval = setInterval(() => {
+        setTime((prevTime) => prevTime + 1);
+        displayCurrentTime();
+      }, 1000);
+    }
     return () => clearInterval(interval);
-  }, []);
+  }, [paused]);
 
-  // ตรวจสอบว่าปัญหาสามารถแก้ไขได้หรือไม่
+  const startGame = () => {
+    setPaused(false);
+  };
+
+  const move = (clickedIndex) => {
+    if (paused) {
+      startGame();
+    }
+
+    const emptyIndex = puzzle.indexOf("Empty");
+
+    if (isAdjacent(emptyIndex, clickedIndex)) {
+      const updatedPuzzle = [...puzzle];
+      [updatedPuzzle[emptyIndex], updatedPuzzle[clickedIndex]] = [
+        updatedPuzzle[clickedIndex],
+        updatedPuzzle[emptyIndex],
+      ];
+
+      setPuzzle(updatedPuzzle);
+
+      if (isSolved(updatedPuzzle)) {
+        recordScore();
+      }
+    }
+  };
+
+  const isAdjacent = (index1, index2) => {
+    const rowSize = 4;
+    const areInSameRow =
+      Math.floor(index1 / rowSize) === Math.floor(index2 / rowSize);
+    const areInSameColumn = index1 % rowSize === index2 % rowSize;
+
+    return (
+      (areInSameRow && Math.abs(index1 - index2) === 1) ||
+      (areInSameColumn && Math.abs(index1 - index2) === rowSize)
+    );
+  };
+
+  const isSolved = (currentPuzzle) => {
+    for (let i = 0; i < currentPuzzle.length - 1; i++) {
+      if (currentPuzzle[i] !== i + 1) {
+        return false;
+      }
+    }
+    return true;
+  };
+
+  const recordScore = () => {
+    const newScore = time + score;
+    setScore(newScore);
+
+    setRank([
+      {
+        score: newScore,
+        time: time,
+      },
+      ...rank,
+    ]);
+  };
+
+  const shufflePuzzle = () => {
+    const shuffledPuzzle = [...puzzle].sort(() => Math.random() - 0.5);
+    if (!isSolvable(shuffledPuzzle)) {
+      shufflePuzzle();
+    } else {
+      setPuzzle(shuffledPuzzle);
+    }
+  };
+
+  const displayCurrentTime = () => {
+    const minutes = Math.floor(time / 60);
+    const seconds = time - minutes * 60;
+    // You can update the time display as needed
+    console.log(`Time: ${convert(minutes)}:${convert(seconds)}`);
+  };
+
   const isSolvable = (puzzle) => {
     const inversions = puzzle.reduce((count, curr, i) => {
       for (let j = i + 1; j < puzzle.length; j++) {
@@ -50,80 +130,16 @@ const Board = () => {
     return inversions % 2 === 0;
   };
 
-  // สุ่มจัดเรียงรูป
-  const shufflePuzzle = () => {
-    const shuffledPuzzle = [...puzzle].sort(() => Math.random() - 0.5);
-    if (!isSolvable(shuffledPuzzle)) {
-      // If the puzzle is not solvable, shuffle again
-      shufflePuzzle();
-    } else {
-      setPuzzle(shuffledPuzzle);
-    }
-  };
-
-  // ย้าย tile และตรวจสอบว่าเรียงถูกต้องหรือไม่
-  const move = (clickedIndex) => {
-    const emptyIndex = puzzle.indexOf("Empty");
-
-    // Check if the clicked tile is adjacent to the empty tile
-    if (isAdjacent(emptyIndex, clickedIndex)) {
-      const updatedPuzzle = [...puzzle];
-      [updatedPuzzle[emptyIndex], updatedPuzzle[clickedIndex]] = [
-        updatedPuzzle[clickedIndex],
-        updatedPuzzle[emptyIndex],
-      ];
-
-      setPuzzle(updatedPuzzle);
-
-      // Check if the puzzle is solved
-      if (isSolved(updatedPuzzle)) {
-        recordScore();
-      }
-    }
-  };
-
-  // ตรวจสอบว่า tile ที่ถูกคลิกอยู่ใกล้ empty หรือไม่
-  const isAdjacent = (index1, index2) => {
-    const rowSize = 4; // Adjust this based on your puzzle layout
-    const areInSameRow =
-      Math.floor(index1 / rowSize) === Math.floor(index2 / rowSize);
-    const areInSameColumn = index1 % rowSize === index2 % rowSize;
-
-    return (
-      (areInSameRow && Math.abs(index1 - index2) === 1) ||
-      (areInSameColumn && Math.abs(index1 - index2) === rowSize)
-    );
-  };
-
-  // ตรวจสอบว่าเรียงรูปถูกต้องหรือไม่
-  const isSolved = (currentPuzzle) => {
-    for (let i = 0; i < currentPuzzle.length - 1; i++) {
-      if (currentPuzzle[i] !== i + 1) {
-        return false;
-      }
-    }
-    return true;
-  };
-
-  // บันทึกคะแนน
-  const recordScore = () => {
-    const newScore = time + score;
-    setScore(newScore);
-
-    // Use spread syntax to create a new array
-    setRank([
-      {
-        score: newScore,
-        time: time,
-      },
-      ...rank,
-    ]);
-  };
+  const convert = (value) => (value < 10 ? `0${value}` : value);
 
   return (
     <div className="App">
       <header>
         <h1>15puzzle</h1>
+        <div className="timer">
+          เวลา: {convert(Math.floor(time / 60))}:{convert(time % 60)}
+        </div>
+        <div className="move-sum">move: {score}</div>
       </header>
       <div className="puzzle">
         {puzzle.map((item, index) => (
@@ -136,9 +152,9 @@ const Board = () => {
           </div>
         ))}
       </div>
-      <div className="timer">เวลา: {time} วินาที</div>
-      <div className="score">คะแนน: {score}</div>
       <button onClick={recordScore}>บันทึกคะแนน</button>
+      <button onClick={startGame}>เริ่มเกม</button>{" "}
+      {/* Add a button to start the game */}
       <ul className="rank">
         {rank.map((item, index) => (
           <li key={index}>
@@ -149,5 +165,8 @@ const Board = () => {
     </div>
   );
 };
+
+// Helper function to convert single-digit minutes/seconds to double digits
+const convert = (value) => (value < 10 ? `0${value}` : value);
 
 export default Board;
