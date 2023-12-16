ba7fb15c8ba010ed65495e6a1f3bbb7a305c6418
Thiraphan24 <thiraphan.codercamp@gmail.com>
2023-12-16 15:32:13 +0700

    feat: add register page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,15 @@
 import { useState } from "react";
 import "./App.css";
 import Board from "./component/board";
+import Register from "./component/Register";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div>
-      <Board />
+      {/* <Board /> */}
+      <Register />
     </div>
   );
 }
